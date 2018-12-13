2e4798bb01c2621dcebaee388742a623af959666
HirotoKawakami <hk10soccer@gmail.com>
2018-12-13 14:46:08 +0900

    edit meta tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,18 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: 'ラヴちゃん Official Web Site | Lafary',
+      site: 'ラヴリーエンジェル♡ | ラヴちゃん',
       reverse: true,
       charset: 'utf-8',
-      description: 'サルーでは毎日の晩酌や、たまの店飲みを他のお酒好き達とシェアできます。まだ知らないお酒や、新しい飲み仲間に出会えます。',
+      description: 'ときめきラヴリー♡夢見るうさぎのおんなのこです♡',
       keywords: 'かわいい,ゆめかわ,ゆめかわいい,ピンク,女の子,キャラクター,グッズ,うさぎ,ぬいぐるみ,サンリオ,サンエックス,ときめき,ラヴリー,メルヘン,ファンシー,カルチャー,魔法,キラキラ,ふわふわ',
       canonical: request.original_url,
       separator: '|',
       icon: image_url('/love_icon-01.png'),
       og: {
-        site_name: 'ラヴちゃん Official Web Site | Lafary',
+        site_name: 'ラヴリーエンジェル♡ | ラヴちゃん',
         title: 'ラヴちゃん Official Web Site | Lafary',
-        description: 'サルーでは毎日の晩酌や、たまの店飲みを他のお酒好き達とシェアできます。まだ知らないお酒や、新しい飲み仲間に出会えます。',
+        description: 'ときめきラヴリー♡夢見るうさぎのおんなのこです♡',
         type: 'website',
         url: request.original_url,
         image: image_url('/love_icon-01.png'),
@@ -20,8 +20,8 @@ module ApplicationHelper
       },
       twitter: {
         card: 'summary_large_image',
-        title: 'ラヴちゃん Official Web Site | Lafary',
-        description: 'サルーでは毎日の晩酌や、たまの店飲みを他のお酒好き達とシェアできます。まだ知らないお酒や、新しい飲み仲間に出会えます。',
+        title: 'ラヴリーエンジェル♡ | ラヴちゃん',
+        description: 'ときめきラヴリー♡夢見るうさぎのおんなのこです♡',
         image: image_url('/love_icon-01.png')
       }
     }
