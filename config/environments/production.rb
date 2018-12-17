55f4a069984d8456d51e9868483bc7262392a60b
HirotoKawakami <hk10soccer@gmail.com>
2018-12-17 14:26:06 +0900

    add google analitics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  GA.tracker = "UA-84704721-5"
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
